7462cb3080430f488be9a10ae01ea650c400c910
Damian Joseph <damianvjoseph@gmail.com>
2022-03-24 04:12:13 +1100

    add data

--- get-afl-data.R
+++ get-afl-data.R
@@ -14,7 +14,38 @@ set_lists_to_chars <- function(x) {
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+
+
+
+
+
+
+
+
+
+
 # get historical afl match results
+
+
+results_2012_temp <- fitzRoy::fetch_results_afl(season = 2012, round_number = NULL, comp = "AFLM")
+results_2012 <- data.frame(lapply(results_2012_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(results_2012,"data//afl_results_2012.csv", row.names = FALSE)
+
+results_2013_temp <- fitzRoy::fetch_results_afl(season = 2013, round_number = NULL, comp = "AFLM")
+results_2013 <- data.frame(lapply(results_2013_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(results_2013,"data//afl_results_2013.csv", row.names = FALSE)
+
+results_2014_temp <- fitzRoy::fetch_results_afl(season = 2014, round_number = NULL, comp = "AFLM")
+results_2014 <- data.frame(lapply(results_2014_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(results_2014,"data//afl_results_2014.csv", row.names = FALSE)
+
+results_2015_temp <- fitzRoy::fetch_results_afl(season = 2015, round_number = NULL, comp = "AFLM")
+results_2015 <- data.frame(lapply(results_2015_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(results_2015,"data//afl_results_2015.csv", row.names = FALSE)
+
+results_2016_temp <- fitzRoy::fetch_results_afl(season = 2016, round_number = NULL, comp = "AFLM")
+results_2016 <- data.frame(lapply(results_2016_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(results_2016,"data//afl_results_2016.csv", row.names = FALSE)
 
 results_2017_temp <- fitzRoy::fetch_results_afl(season = 2017, round_number = NULL, comp = "AFLM")
 results_2017 <- data.frame(lapply(results_2017_temp, set_lists_to_chars), stringsAsFactors = F)
@@ -40,13 +71,16 @@ results_2022_temp <- fitzRoy::fetch_results_afl(season = 2022, round_number = NU
 results_2022 <- data.frame(lapply(results_2022_temp, set_lists_to_chars), stringsAsFactors = F)
 write.csv(results_2022,"data//afl_results_2022.csv", row.names = FALSE)
 
+
 fixture_2022_temp <- fitzRoy::fetch_fixture_afl(season = 2022, round_number = NULL, comp = "AFLM")
 fixture_2022 <- data.frame(lapply(fixture_2022_temp, set_lists_to_chars), stringsAsFactors = F)
 write.csv(fixture_2022,"data//fixture_2022.csv", row.names = FALSE)
 
 
-# afltables_stats_temp <- fitzRoy::fetch_player_stats_afltables(season = 2021, round_number = NULL)
-# afltables_stats <- data.frame(lapply(afltables_stats_temp, set_lists_to_chars), stringsAsFactors = F)
-# write.csv(afltables_stats,"data//afltables_stats_2021.csv", row.names = FALSE)
+afltables_stats_temp <- fitzRoy::fetch_player_stats_afltables(season = NULL, round_number = NULL)
+afltables_stats <- data.frame(lapply(afltables_stats_temp, set_lists_to_chars), stringsAsFactors = F)
+write.csv(afltables_stats,"data//afl_players_stats.csv", row.names = FALSE)
+
+
 
 
